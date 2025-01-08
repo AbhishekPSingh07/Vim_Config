2cb364eda0f61557f0c3fb24e5403c834aec2310
AbhishekPSingh07 <abhisheksinghas1205@gmail.com>
2025-01-08 23:47:44 +0530

    config changes for java lsp

--- lua/plugins/java_lsp.lua
+++ lua/plugins/java_lsp.lua
@@ -1,26 +1,14 @@
-return {
-  {
-    "neovim/nvim-lspconfig",
-    opts = {
-      servers = {
-        jdtls = {
-          cmd = {
-            "jdtls", -- Ensure this points to the correct jdtls binary if it's not globally available
-          },
-          root_dir = require("lspconfig.util").root_pattern(".git", "mvnw", "gradlew", "pom.xml", "build.gradle"),
-          settings = {
-            java = {
-              home = "/Library/Java/JavaVirtualMachines/jdk-20.jdk/Contents/Home", -- Optional: Specify your Java home if needed
-            },
-          },
-          on_attach = function(client, bufnr)
-            -- Keybindings and other LSP features can be configured here
-            local opts = { buffer = bufnr }
-            vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
-            vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
-          end,
-        },
-      },
+require("lspconfig").jdtls.setup({
+  cmd = { "jdtls" },
+  root_dir = require("lspconfig.util").root_pattern(".git", "mvnw", "gradlew", "pom.xml", "build.gradle"),
+  settings = {
+    java = {
+      home = "/Library/Java/JavaVirtualMachines/jdk-21.jdk/Contents/Home",
     },
   },
-}
+  on_attach = function(client, bufnr)
+    local bufopts = { noremap = true, silent = true, buffer = bufnr }
+    vim.keymap.set("n", "gd", vim.lsp.buf.definition, bufopts)
+    vim.keymap.set("n", "K", vim.lsp.buf.hover, bufopts)
+  end,
+})
---
+++ lua/plugins/lspProgresConfig.lua
@@ -0,0 +1,10 @@
+return {
+  {
+    "j-hui/fidget.nvim", -- Example of an LSP progress plugin
+    tag = "legacy", -- Or your specific plugin version
+    opts = {
+      window = { blend = 0 }, -- Optional: Adjust transparency
+    },
+    enabled = false, -- Disable the plugin temporarily
+  },
+}
